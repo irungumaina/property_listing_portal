ea199d5c3c03cd4610d20a5b50105be1b9c23d1a
irungumaina <dev.irungu@gmail.com>
2021-12-02 12:03:11 +0300

    created listing detail page html

--- listing/urls.py
+++ listing/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ListingListView, ListingDetailView
+from .views import ListingListView, ListingDetailView, ListingCreateView, ListingUpdateView
 
 urlpatterns = [
     path('', ListingListView.as_view(), name='listing_list'),
     path('<uuid:pk>', ListingDetailView.as_view(), name='listing_detail'),
+    path('<uuid:pk>', ListingCreateView.as_view(), name='listing_create'),
+    path('<uuid:pk>', ListingUpdateView.as_view(), name='listing_update'),
 ]
--- listing/views.py
+++ listing/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+# from django.views import generic
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Listing
 
@@ -14,3 +16,17 @@ class ListingDetailView(DetailView):
     model = Listing
     context_object_name = 'listing'
     template_name = 'listings/listing_detail.html'
+
+
+class ListingCreateView(CreateView):
+    model = Listing
+    success_url = reverse_lazy('listing_detail')
+    context_object_name = 'listing_create'
+    template_name = 'listings/listing_create.html'
+
+
+class ListingUpdateView(UpdateView):
+    model = Listing
+    success_url = reverse_lazy('listing_detail')
+    context_object_name = 'listing_update'
+    template_name = 'listings/listing_update.html'
